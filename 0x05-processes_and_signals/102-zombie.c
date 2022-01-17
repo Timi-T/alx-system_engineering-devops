12a3ea5ac510e7dba5ab68d4b97398db94b63022
Timi-T <timiopebode@gmail.com>
2022-01-17 06:23:24 -0800

    exit child process

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 int infinite_while(void);
 /**
@@ -16,12 +17,18 @@ int main(void)
 
 	ret_val = fork();
 	if (ret_val == 0)
+	{
 		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+	}
 	while (i < 4 && ret_val != 0)
 	{
 		ret_val = fork();
 		if (ret_val == 0)
+		{
 			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
 		i++;
 	}
 	infinite_while();
