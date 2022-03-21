885ba74b029c91303fe708bc83746ad98e4b30e1
Timi-T <timiopebode@gmail.com>
2022-03-21 18:41:08 +0000

    exec for puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,6 @@
 #configuration for header response
 
-$str='frontend http_front
-	bind *:80
-	stats uri /haproxy?stats
-	default_backend http_back
-backend http_back
-	balance roundrobin
-	server 1919-web-01 34.148.87.245:80 check
-	server 1919-web-02 3.231.218.82:80 check'
-file_line {'/etc/haproxy/haproxy.cfg':
-	ensure => present,
-	path   => 'etc/haproxy/haproxy.cfg',
-	line   => $str
+exec { 'HTTP response header':
+	command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; sudo ufw --force enable; sudo ufw allow "Nginx HTTP"; sudo ufw allow 22; sudo sed -i "s/server_name _;/server_name roadsidedev.tech;\n\trewrite ^\/redirect_me https:\/\/youtube.com permanent;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default; sudo service nginx restart;',
+	provider => shell,
 }
