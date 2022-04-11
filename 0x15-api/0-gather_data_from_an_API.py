7379722af1d14f1a39869c7cfffee8ebe24ae8de
Timi-T <timiopebode@gmail.com>
2022-04-11 11:27:38 +0100

    if name is main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,25 +8,25 @@ import json
 import sys
 
 
-id = int(sys.argv[1])
-url_users = 'https://jsonplaceholder.typicode.com/users?id={}'.format(id)
-url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-user_response = requests.get(url_users)
-todo_response = requests.get(url_todo)
+if __name__ == "__main__":
+    id = int(sys.argv[1])
+    users = 'https://jsonplaceholder.typicode.com/users?id={}'.format(id)
+    todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+    user_response = requests.get(users)
+    todo_response = requests.get(todo)
 
+    data = todo_response.text
+    tasks = json.loads(data)
+    user = user_response.text
+    user = json.loads(user)
+    name = user[0].get('name')
 
-data = todo_response.text
-tasks = json.loads(data)
-user = user_response.text
-user = json.loads(user)
-name = user[0].get('name')
-
-completed = 0
-total = len(tasks)
-for task in tasks:
-    if task.get('completed') is True:
-        completed += 1
-print("Employee {} is done with tasks ({}/{})".format(name, completed, total))
-for task in tasks:
-    if task.get('completed') is True:
-        print("\t{}".format(task.get('title')))
+    complt = 0
+    all = len(tasks)
+    for task in tasks:
+        if task.get('completed') is True:
+            complt += 1
+    print("Employee {} is done with tasks ({}/{})".format(name, complt, all))
+    for task in tasks:
+        if task.get('completed') is True:
+            print("\t{}".format(task.get('title')))
