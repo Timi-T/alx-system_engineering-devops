fa14ef72e4561c5f58553b582ef567d1cad264b0
Timi-T <timiopebode.gmail>
2022-04-27 16:14:10 +0000

    remove excess comments

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,4 @@
 #puppet manifest to rename a file and create a file
-
-#exec {"mv class-wp-locale.php class-wp-locale.phpp":
-	#command => "mv class-wp-locale.php class-wp-locale.phpp",
-#        path => "/var/www/html/wp-includes"
-#}
-
 file {'/var/www/html/wp-includes/class-wp-locale.phpp':
 	ensure => present,
         source => "/var/www/html/wp-includes/class-wp-locale.php",
