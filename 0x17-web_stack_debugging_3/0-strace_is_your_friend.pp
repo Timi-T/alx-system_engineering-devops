ce3cae8c0f2119296a7b500f794f3b457adba824
Timi-T <timiopebode.gmail>
2022-04-27 15:51:57 +0000

    chnged syntax

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #puppet manifest to rename a file and create a file
 
-exec {
-	'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
-	'touch /var/www/html/wp-content/db.php'
+exec {"/var/www/html/wp-includes/class-wp-locale.php":
+	command => "mv class-wp-locale.php class-wp-locale.phpp",
+        path => "/var/www/html/wp-includes"
 }
