520e75e5a8b8c2f57976f8e9ace8307bb6ef7c09
Timi-T <timiopebode.gmail>
2022-04-27 16:12:14 +0000

    copy to a file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,11 @@
 #puppet manifest to rename a file and create a file
 
-exec {"/var/www/html/wp-includes/class-wp-locale.php":
-	command => "mv class-wp-locale.php class-wp-locale.phpp",
-        path => "/var/www/html/wp-includes"
+#exec {"mv class-wp-locale.php class-wp-locale.phpp":
+	#command => "mv class-wp-locale.php class-wp-locale.phpp",
+#        path => "/var/www/html/wp-includes"
+#}
+
+file {'/var/www/html/wp-includes/class-wp-locale.phpp':
+	ensure => present,
+        source => "/var/www/html/wp-includes/class-wp-locale.php",
 }
