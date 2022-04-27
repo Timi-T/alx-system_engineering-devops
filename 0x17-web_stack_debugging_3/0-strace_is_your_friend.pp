f63162e337606f571f301964cf8e938ef15a3d7a
Timi-T <timiopebode.gmail>
2022-04-27 16:22:12 +0000

    puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #puppet manifest to rename a file and create a file
 file {'/var/www/html/wp-includes/class-wp-locale.phpp':
-	ensure => present,
-        source => "/var/www/html/wp-includes/class-wp-locale.php",
+  ensure => present,
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
 }
